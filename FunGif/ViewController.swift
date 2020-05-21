4e118fe6f9550911afc455f6525ee9f3e260f7db
Artem <jester.b1@mail.ru>
2020-05-21 23:50:48 +0300

    Initial Commit

---
+++ FunGif/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunGif
+//
+//  Created by Indoril Nerevar on 5/21/20.
+//  Copyright © 2020 Artem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
