f5c9d2588dc3717357b16cf49510a7339822ca70
Artem <jester.b1@mail.ru>
2020-05-24 03:46:19 +0300

    Adding is new enums

---
+++ FunGif/Logic.swift
@@ -0,0 +1,14 @@
+//
+//  Logic.swift
+//  FunGif
+//
+//  Created by Indoril Nerevar on 5/24/20.
+//  Copyright © 2020 Artem. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Logic {
+    case rock, scissors, paper
+}
